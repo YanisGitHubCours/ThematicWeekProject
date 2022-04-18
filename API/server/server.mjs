d2b5ffadc7bcee85bad61fc76b3d1917ae1b7d03
Yanis <yanis@Yone.local>
2022-04-18 17:40:02 +0200

    server file

---
+++ API/server/server.mjs
@@ -0,0 +1,21 @@
+//import router from './routes.mjs'
+import express from 'express'
+import bodyparser from 'body-parser'
+import morgan from 'morgan'
+//import swaggerUI from 'swagger-ui-express'
+//import docs from '../docs/index.mjs'
+import env from 'dotenv'
+
+env.config()
+const port = process.env.PORT
+const app = express()
+
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+app.use(morgan('tiny'))
+//app.use(router)
+//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs))
+
+app.listen(port, () => {
+    console.log(`server listen at ${port}`)
+})
