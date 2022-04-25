e0bf559674223a3f077c2bdc982552777f1509bc
Yanis <yanis@Yone.local>
2022-04-25 23:28:53 +0200

    CRUD list contact

---
+++ API/server/routes/list/list.mjs
@@ -0,0 +1,57 @@
+import express from 'express'
+import { MongoClient, ObjectId } from 'mongodb';
+import 'dotenv/config'
+const app = express.Router()
+
+let database = "EmailingDb"
+let collection = "list"
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+await client.connect();
+
+(async () => {
+    app.get('/list/:id', async (req, res) => {
+
+        const id = ObjectId(req.params.id)
+
+        const response = await client.db(database).collection(collection).find({ "_id": id }).toArray()
+
+        res.send(response)
+    });
+
+    app.delete('/list/:id', async (req, res) => {
+
+        const id = ObjectId(req.params.id)
+
+        const response = await client.db(database).collection(collection).deleteOne({ "_id": id });
+
+        res.send(response)
+
+    })
+
+    app.post('/list', async (req, res) => {
+        const json = req.body;
+        const response = await client.db(database).collection(collection).insertOne(json);
+
+        res.send(response)
+    });
+
+    app.patch('/list/:id', async (req, res) => {
+        const id = ObjectId(req.params.id);
+        const json = req.body;
+
+        const response = await client.db(database).collection(collection).updateOne({ "_id": id }, { $set: { "text": json.text } });
+
+        res.send(response);
+    });
+})();
+
+
+export default app
+
